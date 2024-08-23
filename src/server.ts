41903ff9a857afc003a9d5504c83b7e75ed400c9
andre <andre.alves@ccc.ufcg.edu.br>
2024-08-23 16:55:00 -0300

    adding routes for budget and incomes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import UserRoutes from './routes/UserRoutes';
 import ExpenseRoutes from './routes/ExpenseRoutes';
+import IncomeRoutes from './routes/IncomeRoutes';
+import BudgetRoutes from './routes/BudgetRoutes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/users', UserRoutes);
-app.use('/expenses', ExpenseRoutes);
+app.use('/api', UserRoutes);
+app.use('/api', ExpenseRoutes);
+app.use('/api', IncomeRoutes);
+app.use('/api', BudgetRoutes);
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
